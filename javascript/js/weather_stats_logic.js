b5f54bbfb2ca50bab51c81d37c52720ec1ed8310
Vincent_Passanisi <vince@santafeimporters.com>
2023-02-09 21:15:46 -0800

    Update

--- javascript/js/weather_stats_logic.js
+++ javascript/js/weather_stats_logic.js
@@ -17,59 +17,47 @@ units = "imperial"
 queryUrl = `${url}appid=${apiKey}&units=${units}&q=`
 
 // Get latitude and temperature for cities
-cities = ['Montgomery', 'Juneau', 'Phoenix', 'Little Rock',	'Sacramento', 'Denver', 'Hartford', 'Dover', 'Honolulu', 'Tallahassee', 'Atlanta',
+cities = ['Montgomery', 'Juneau', 'Phoenix', 'Little Rock', 'Sacramento', 'Denver', 'Hartford', 'Dover', 'Honolulu', 'Tallahassee', 'Atlanta',
             'Boise', 'Springfield', 'Indianapolis', 'Des Moines', 'Topeka', 'Frankfort', 'Baton Rouge', 'Augusta', 'Annapolis', 'Boston', 
             'Lansing', 'St. Paul', 'Jackson', 'Jefferson City', 'Helena', 'Lincoln', 'Carson City', 'Concord', 'Trenton', 'Santa Fe', 'Raleigh', 
             'Bismarck', 'Albany', 'Columbus', 'Oklahoma City', 'Salem', 'Harrisburg', 'Providence', 'Columbia', 'Pierre', 'Nashville', 'Austin', 
             'Salt Lake City', 'Montpelier', 'Richmond', 'Olympia', 'Charleston', 'Madison', 'Cheyenne']
 
-// set up lists to hold reponse info
-// lat = []
-// lon = []
-// temp = []
-// descr = []
-weatherDict = {}
-// console.log(cities.length);
-//  Loop through the list of cities and perform a request for data on each
+// set up lists to hold response info
+lat = []
+lon = []
+temp = []
+descr = []
+weatherData = []
+
+// Loop through the list of cities and perform a request for data on each
 for (i = 0; i < cities.length; i++) {
-  let location = [];
   d3.json(queryUrl + cities[i]).then(function (data) {
-    // response = requests.get(query_url + city).json()
-    // lat.push(data['coord']['lat'])
-    // lon.push(data['coord']['lon'])
-    // temp.push(data['main']['temp'])
-    // descr.push(data['weather'][0]['description'])
-    weatherDict[i] = {
+    lat.push(data['coord']['lat'])
+    lon.push(data['coord']['lon'])
+    temp.push(data['main']['temp'])
+    descr.push(data['weather'][0]['description'])
+
+    weatherData.push({
       city: data['name'],
       lat: data['coord']['lat'],
       lon: data['coord']['lon'],
       temp: data['main']['temp'],
       descr: data['weather'][0]['description']
+    });
+
+    // console.log the weatherData after all requests have been processed
+    if (weatherData.length == cities.length) {
+      for (let x = 0; x < weatherData.length; x++) {
+        let cityWeather = weatherData[x];
+        let location = [];
+        location.push(cityWeather.lat);
+        location.push(cityWeather.lon);
+        console.log(cityWeather);
+        L.marker(location)
+          .bindPopup(`<h1>${cityWeather.city}</h1> <hr> <h3>Current Temperature: ${cityWeather.temp}</h3> <hr> <h3>Current Weather: ${cityWeather.descr}</h3>`).addTo(myMap);
+      };
     };
-
-    // console.log(weatherDict[i]);
-})};
-// console.log(weatherDict.length);
-// let weatherDict = {
-//   "city": cities,
-//   "lat": lat,
-//   'lon': lon,
-//   "temp": temp,
-//   'description': descr
-// };
-
-console.log(weatherDict[0]);
-
-// Looping through the weatherDict array, create one marker for each state, bind a popup containing up-to-date weather info, and add it to the map.
-// for (let x = 0; x < 50; x++) {
-//   let cityWeather = weatherDict[x];
-//   let location = [];
-//   location.push(cityWeather[x].lat);
-//   location.push(cityWeather[x].lon);
-  // console.log(location);
-  // L.marker(location)
-    // .bindPopup(`<h1>${i.city}</h1> <hr> <h3>Current Temperature: ${i.temp}</h3> <hr> <h3>Current Weather: ${i.description}</h3>`).addTo(myMap);
-// };
-
-// console.log(weatherDict);
+  });
+}
 
