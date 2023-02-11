fa5307439ee67c73659df0021a66712f288c9d1e
davidixonn <113266571+davidixonn@users.noreply.github.com>
2023-02-11 14:54:38 -0800

    Updating SQLite database & Flask

--- javascript/app.py
+++ javascript/app.py
@@ -1,23 +1,85 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
 from flask import Flask, jsonify
-import os
-import psycopg2
-from dotenv import load_dotenv
 
-load_dotenv()
 
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///weatherlite.db")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(autoload_with=engine)
+
+# Save reference to the table
+Passenger = Base.classes.passenger
+
+#################################################
+# Flask Setup
+#################################################
 app = Flask(__name__)
-url = os.environ.get("DATABASE_URL")  # gets variables from environment
-connection = psycopg2.connect(url)
 
 
-# @app.route("/")
-# def index():
-#    conn = psycopg2.connect("postgresql://postgres:root@localhost:5432/postgres")
-#    return 'it works'
+#################################################
+# Flask Routes
+#################################################
 
-@app.route("/start/", methods=['get', 'post'])
+@app.route("/")
 def welcome():
-   return 'it works'
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/names<br/>"
+        f"/api/v1.0/passengers"
+    )
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=105)
+
+@app.route("/api/v1.0/names")
+def names():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all passenger names"""
+    # Query all passengers
+    results = session.query(Passenger.name).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    all_names = list(np.ravel(results))
+
+    return jsonify(all_names)
+
+
+@app.route("/api/v1.0/passengers")
+def passengers():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of passenger data including the name, age, and sex of each passenger"""
+    # Query all passengers
+    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_passengers
+    all_passengers = []
+    for name, age, sex in results:
+        passenger_dict = {}
+        passenger_dict["name"] = name
+        passenger_dict["age"] = age
+        passenger_dict["sex"] = sex
+        all_passengers.append(passenger_dict)
+
+    return jsonify(all_passengers)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
